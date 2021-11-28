75c661eca72c80a712ff10f5cd84a978a56f7103
Gabriel Donato <gabrielcdonato@gmail.com>
2021-11-28 20:36:21 -0300

    fix

--- lib/test/data/usecases/remote_authentication_test.dart
+++ lib/test/data/usecases/remote_authentication_test.dart
@@ -9,24 +9,30 @@ class RemoteAuthentication {
 
   RemoteAuthentication({@required this.httpClient, @required this.url});
 
-  Future<void> auth() async {}
+  Future<void> auth() async {
+    await httpClient.request(url: url, method: 'get');
+  }
 }
 
 abstract class HttpClient {
   Future<void> request({
     @required String url,
+    @required String method,
   });
 }
 
 class HttpClientSpy extends Mock implements HttpClient {}
 
 void main() {
-  test('Should call HttpClient with correct URL', () async {
+  test('Should call HttpClient with correct values', () async {
     final httpClient = HttpClientSpy();
     final url = faker.internet.httpUrl();
     final sut = RemoteAuthentication(httpClient: httpClient, url: url);
 //sut significa  system under test    await sut.auth();
 
-    verify(httpClient.request(url: url));
+    verify(httpClient.request(
+      url: url,
+      method: 'post',
+    ));
   });
 }
