c363da7dd5e3886423eac49ecf8fe9d458de5681
Gabriel Donato <gabrielcdonato@gmail.com>
2021-11-28 23:52:07 -0300

    ensure RemoteAuthentication throws UnexpectedError on 500

--- lib/test/data/usecases/remote_authentication_test.dart
+++ lib/test/data/usecases/remote_authentication_test.dart
@@ -33,12 +33,12 @@ void main() {
         body: {'email': params.email, 'password': params.secret}));
   });
 
-  test('Should throw UnexpectedError if HttpClient returns 404', () async {
+  test('Should throw UnexpectedError if HttpClient returns 500', () async {
     when(httpClient.request(
             url: anyNamed('url'),
             method: anyNamed('method'),
             body: anyNamed('body')))
-        .thenThrow(HttpError.notFound);
+        .thenThrow(HttpError.serverError);
 
     final future = sut.auth(params);
 
